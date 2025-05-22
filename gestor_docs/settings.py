26273bb84d6fc55e335be175fb3bd4ccdab2486c
BenjaBarraza <ben.barraza@duocuc.cl>
2025-05-22 00:14:10 -0400

    Actualizar ALLOWED_HOSTS en settings.py para corregir la configuración de producción

--- gestor_docs/settings.py
+++ gestor_docs/settings.py
@@ -26,10 +26,8 @@ SECRET_KEY = 'django-insecure-i=$f##zm$9fx0zh+xz+enq$7ha)o6pvr&xw+pci7sdaj+7ajfw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 
-                 'localhost', 
-                 'gestor-documental-c1tp.onrender.com/', 
-                 'render.com']
+ALLOWED_HOSTS = ['gestor-documental-c1tp.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
