81bf6374974f3fa3fdec34bd3e48c1777e67cd31
BenjaBarraza <ben.barraza@duocuc.cl>
2025-06-09 13:37:43 -0400

    borrado cs de middleware

--- gestor_docs/settings.py
+++ gestor_docs/settings.py
@@ -163,7 +163,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'csp.middleware.CSPMiddleware',  # Middleware para Content Security Policy
+    
 
 ]
 
